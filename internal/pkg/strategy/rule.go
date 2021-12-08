2230ebf4f7de3eb4c7fe68f129c25e441da510c2
Phat Huynh <phat@kakao.com>
2021-12-08 01:12:08 +0000

    Update strategy rule

--- internal/pkg/strategy/rule.go
+++ internal/pkg/strategy/rule.go
@@ -1,20 +1,17 @@
 package strategy
 
 import (
-	"time"
-
 	ta "github.com/itsphat/techan"
 	"github.com/sdcoffey/big"
 
 	"follow.market/internal/pkg/runner"
-	tax "follow.market/internal/pkg/techanex"
 )
 
 type GenericRule struct {
 	Signal Signal
 
 	runner *runner.Runner
-	trade  *tax.Trade
+	//trade  *tax.Trade
 }
 
 func NewRule(signal Signal) *GenericRule {
@@ -22,20 +19,28 @@ func NewRule(signal Signal) *GenericRule {
 }
 
 func (gr *GenericRule) SetRunner(r *runner.Runner) *GenericRule {
+	if gr == nil {
+		return nil
+	}
 	gr.runner = r
 	return gr
 }
 
-func (gr *GenericRule) SetTrade(t *tax.Trade) *GenericRule {
-	gr.trade = t
-	return gr
-}
+//func (gr *GenericRule) SetTrade(t *tax.Trade) *GenericRule {
+//	gr.trade = t
+//	return gr
+//}
 
 func (gr GenericRule) IsSatisfied(index int, record *ta.TradingRecord) bool {
-	if gr.runner == nil && gr.trade == nil {
+	// the index is supposed to range from 0 to len(r.line.Candles)
+	if gr.runner == nil { //&& gr.trade == nil {
 		return false
 	}
-	return gr.Signal.Evaluate(gr.runner, gr.trade)
+	if gr.Signal.IsOnTrade() {
+		return false
+	}
+	//return gr.Signal.Evaluate(gr.runner, gr.trade)
+	return gr.Signal.Evaluate(gr.runner, nil)
 }
 
 type StopLossRule struct {
@@ -45,6 +50,9 @@ type StopLossRule struct {
 }
 
 func (sr *StopLossRule) SetRunner(r *runner.Runner) *StopLossRule {
+	if sr == nil {
+		return nil
+	}
 	sr.runner = r
 	return sr
 }
@@ -60,7 +68,7 @@ func (sr StopLossRule) IsSatisfied(index int, record *ta.TradingRecord) bool {
 	if !record.CurrentPosition().IsOpen() {
 		return false
 	}
-	line, ok := sr.runner.GetLines(time.Minute)
+	line, ok := sr.runner.GetLines(sr.runner.SmallestFrame())
 	if !ok || line == nil {
 		return false
 	}
@@ -68,7 +76,7 @@ func (sr StopLossRule) IsSatisfied(index int, record *ta.TradingRecord) bool {
 	if candle == nil {
 		return false
 	}
-	openPrice := record.CurrentPosition().CostBasis()
+	openPrice := record.CurrentPosition().EntranceOrder().Price
 	loss := candle.ClosePrice.Div(openPrice).Sub(big.ONE)
 	return loss.LTE(sr.LossTolerance)
 }
@@ -84,6 +92,9 @@ func NewTakeProfitRule(mgr float64) *TakeProfitRule {
 }
 
 func (pr *TakeProfitRule) SetRunner(r *runner.Runner) *TakeProfitRule {
+	if pr == nil {
+		return nil
+	}
 	pr.runner = r
 	return pr
 }
@@ -95,7 +106,7 @@ func (pr TakeProfitRule) IsSatisfied(index int, record *ta.TradingRecord) bool {
 	if !record.CurrentPosition().IsOpen() {
 		return false
 	}
-	line, ok := pr.runner.GetLines(time.Minute)
+	line, ok := pr.runner.GetLines(pr.runner.SmallestFrame())
 	if !ok || line == nil {
 		return false
 	}
@@ -103,16 +114,32 @@ func (pr TakeProfitRule) IsSatisfied(index int, record *ta.TradingRecord) bool {
 	if candle == nil {
 		return false
 	}
-	openPrice := record.CurrentPosition().CostBasis()
-	loss := candle.ClosePrice.Div(openPrice).Sub(big.ONE)
-	return loss.GTE(pr.ProfitMargin)
+	openPrice := record.CurrentPosition().EntranceOrder().Price
+	profit := candle.ClosePrice.Div(openPrice).Sub(big.ONE)
+	return profit.GTE(pr.ProfitMargin)
 }
 
-func NewRiskRewardRule(lossTolerance, profitMargin float64, r *runner.Runner) ta.Rule {
-	if r == nil {
-		panic("runner must not be nil")
+type RiskRewardRule struct {
+	StopLoss   *StopLossRule
+	TakeProfit *TakeProfitRule
+}
+
+func NewRiskRewardRule(lossTolerance, profitMargin float64) *RiskRewardRule {
+	return &RiskRewardRule{StopLoss: NewStopLossRule(lossTolerance),
+		TakeProfit: NewTakeProfitRule(profitMargin),
 	}
-	sl := NewStopLossRule(lossTolerance).SetRunner(r)
-	tp := NewTakeProfitRule(profitMargin).SetRunner(r)
-	return ta.Or(sl, tp)
+}
+
+func (rr *RiskRewardRule) SetRunner(r *runner.Runner) *RiskRewardRule {
+	if rr == nil {
+		return nil
+	}
+	rr.StopLoss.SetRunner(r)
+	rr.TakeProfit.SetRunner(r)
+	return rr
+}
+
+func (rr RiskRewardRule) IsSatisfied(index int, record *ta.TradingRecord) bool {
+	//return rr.StopLoss.IsSatisfied(index, record) || rr.TakeProfit.IsSatisfied(index, record)
+	return ta.Or(rr.StopLoss, rr.TakeProfit).IsSatisfied(index, record)
 }
