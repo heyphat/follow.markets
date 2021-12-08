36b1863bc2b7d05fd3c9227c40efe1b4adbbbd81
Phat Huynh <phat@kakao.com>
2021-12-08 01:15:56 +0000

    Add basic logic for the tester

--- internal/cmd/market/tester.go
+++ internal/cmd/market/tester.go
@@ -2,15 +2,19 @@ package market
 
 import (
 	"errors"
-	"sync"
+	"fmt"
+	"time"
 
+	"github.com/sdcoffey/big"
+
+	"follow.market/internal/pkg/runner"
+	"follow.market/internal/pkg/strategy"
+	tax "follow.market/internal/pkg/techanex"
 	"follow.market/pkg/log"
+	ta "github.com/itsphat/techan"
 )
 
 type tester struct {
-	sync.Mutex
-	connected bool
-
 	// shared properties with other market participants
 	logger   *log.Logger
 	provider *provider
@@ -21,11 +25,74 @@ func newTester(participants *sharedParticipants) (*tester, error) {
 		return nil, errors.New("missing shared participants")
 	}
 	return &tester{
-		connected: false,
-
 		logger:   participants.logger,
 		provider: participants.provider,
 	}, nil
 }
 
-func (t *tester) test() {}
+type tmember struct {
+	balance  big.Decimal
+	runner   *runner.Runner
+	record   *ta.TradingRecord
+	strategy *strategy.Strategy
+}
+
+func (t *tester) test(ticker string, initBalance big.Decimal, stg *strategy.Strategy) (tmember, error) {
+	if initBalance.LTE(big.ZERO) {
+		return tmember{}, errors.New("init balance has to be > 0")
+	}
+	if stg == nil {
+		return tmember{}, errors.New("missing trading strategy")
+	}
+	r := runner.NewRunner(ticker, &runner.RunnerConfigs{
+		LFrames: []time.Duration{
+			time.Minute * 15,
+			time.Minute * 30,
+			time.Minute * 60,
+			time.Hour * 4,
+			time.Hour * 24,
+		},
+		IConfigs: tax.NewDefaultIndicatorConfigs(),
+	})
+	mem := tmember{
+		runner:   r,
+		record:   ta.NewTradingRecord(),
+		balance:  initBalance,
+		strategy: stg.SetRunner(r),
+	}
+	candles, err := t.provider.fetchBinanceKlines(ticker, time.Minute*15)
+	if err != nil {
+		return mem, err
+	}
+	for i, c := range candles {
+		if !mem.runner.SyncCandle(c) {
+			t.logger.Warning.Println(t.newLog(ticker, "failed to sync new candle on watching"))
+			continue
+		}
+		if mem.strategy.ShouldEnter(i, mem.record) {
+			mem.record.Operate(ta.Order{
+				Side:          stg.EntryRule.Signal.Side(ta.BUY),
+				Price:         c.ClosePrice,
+				Amount:        mem.balance.Div(c.ClosePrice),
+				Security:      ticker,
+				ExecutionTime: c.Period.Start,
+			})
+			continue
+		}
+		if mem.strategy.ShouldExit(i, mem.record) || (mem.record.CurrentPosition().IsOpen() && i == len(candles)-1) {
+			mem.record.Operate(ta.Order{
+				Side:          stg.EntryRule.Signal.Side(ta.SELL),
+				Price:         c.ClosePrice,
+				Amount:        mem.record.CurrentPosition().EntranceOrder().Amount,
+				Security:      ticker,
+				ExecutionTime: c.Period.Start,
+			})
+		}
+	}
+	return mem, nil
+}
+
+// returns a log for the tester
+func (t *tester) newLog(ticker, message string) string {
+	return fmt.Sprintf("[tester] %s: %s", ticker, message)
+}
