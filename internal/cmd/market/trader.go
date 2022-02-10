545d6a0fd76b997d7ab1d6f4d88465bccfa25e86
Phat Huynh <phat@kakao.com>
2022-02-10 13:48:58 +0000

    Add new logics for the trader

--- internal/cmd/market/trader.go
+++ internal/cmd/market/trader.go
@@ -4,15 +4,15 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
 	"sync"
-	"time"
 
 	bn "github.com/adshao/go-binance/v2"
 	bnf "github.com/adshao/go-binance/v2/futures"
 	"github.com/dlclark/regexp2"
+	"github.com/sdcoffey/big"
 
 	"follow.markets/internal/pkg/runner"
-	tax "follow.markets/internal/pkg/techanex"
 	"follow.markets/pkg/config"
 	"follow.markets/pkg/log"
 )
@@ -21,8 +21,20 @@ type trader struct {
 	sync.Mutex
 	connected        bool
 	binSpotListenKey string
+	binSpotBalances  *sync.Map
 	binFutuListenKey string
-	allowedPatterns  []*regexp2.Regexp
+
+	// trader configuration
+	quoteCurrency   string
+	allowedPatterns []*regexp2.Regexp
+	allowedMarkets  []runner.MarketType
+
+	minLeverage   big.Decimal
+	maxLeverage   big.Decimal
+	minBalance    big.Decimal
+	maxPositions  big.Decimal
+	profitMargin  big.Decimal
+	lossTolerance big.Decimal
 
 	// shared properties with other market participants
 	logger       *log.Logger
@@ -31,9 +43,8 @@ type trader struct {
 }
 
 type tdmember struct {
-	runner *runner.Runner
-	//bChann chan *ta.Candle
-	tChann chan *tax.Trade
+	runner   *runner.Runner
+	channels *streamingChannels
 }
 
 func newTrader(participants *sharedParticipants, configs *config.Configs) (*trader, error) {
@@ -41,47 +52,75 @@ func newTrader(participants *sharedParticipants, configs *config.Configs) (*trad
 		return nil, errors.New("missing shared participants or configs")
 	}
 	t := &trader{
-		connected: false,
+		connected:       false,
+		binSpotBalances: &sync.Map{},
+
+		minBalance:    big.ZERO,
+		maxPositions:  big.ZERO,
+		minLeverage:   big.NewDecimal(1.0),
+		maxLeverage:   big.NewDecimal(1.0),
+		lossTolerance: big.NewDecimal(0.01),
+		profitMargin:  big.NewDecimal(0.02),
 
 		logger:       participants.logger,
 		provider:     participants.provider,
 		communicator: participants.communicator,
 	}
-	reges := make([]*regexp2.Regexp, 0)
-	for _, t := range configs.Trader.AllowedPatterns {
-		reg, err := regexp2.Compile(t, 0)
-		if err != nil {
-			return nil, err
-		}
-		reges = append(reges, reg)
-	}
-	t.allowedPatterns = reges
 	var err error
-	t.binSpotListenKey, err = t.provider.binSpot.NewStartUserStreamService().Do(context.Background())
-	if err != nil {
+	if err = t.updateConfigs(configs); err != nil {
+	}
+	if t.binSpotBalances, err = t.provider.fetchBinSpotBalances(); err != nil {
 		return nil, err
 	}
-	t.binFutuListenKey, err = t.provider.binFutu.NewStartUserStreamService().Do(context.Background())
-	if err != nil {
+	if t.binSpotListenKey, t.binFutuListenKey, err = t.provider.fetchBinUserDataListenKey(); err != nil {
 		return nil, err
 	}
-	go func() {
-		for {
-			t.provider.binSpot.NewKeepaliveUserStreamService().ListenKey(t.binSpotListenKey).Do(context.Background())
-			t.provider.binFutu.NewKeepaliveUserStreamService().ListenKey(t.binFutuListenKey).Do(context.Background())
-			time.Sleep(time.Duration(30) * time.Minute)
-		}
-	}()
 	go t.binSpotUserDataStreaming()
 	go t.binFutuUserDataStreaming()
 	return t, nil
+}
+
+// updateConfigs updates basic trading configuration for the trader. It overrides the current configuration.
+func (t *trader) updateConfigs(configs *config.Configs) error {
+	var err error
+	reges := make([]*regexp2.Regexp, len(configs.Market.Trader.AllowedPatterns))
+	for i, p := range configs.Market.Trader.AllowedPatterns {
+		if reges[i], err = regexp2.Compile(p, 0); err != nil {
+			return err
+		}
+	}
+	t.allowedPatterns = reges
+	mkts := make([]runner.MarketType, len(configs.Market.Trader.AllowedMarkets))
+	for i, m := range configs.Market.Trader.AllowedMarkets {
+		if mt, ok := runner.ValidateMarket(m); ok {
+			mkts[i] = mt
+		}
+	}
+	t.allowedMarkets = mkts
+	t.maxPositions = big.NewDecimal(configs.Market.Trader.MaxPositions)
+	t.minBalance = big.NewDecimal(configs.Market.Trader.MinBalance)
+	t.lossTolerance, t.profitMargin = big.NewDecimal(0.01), big.NewDecimal(0.02)
+	if configs.Market.Trader.MinLeverage > 0 {
+		t.minLeverage = big.NewDecimal(configs.Market.Trader.MinLeverage)
+	}
+	if configs.Market.Trader.MaxLeverage > 0 {
+		t.minLeverage = big.NewDecimal(configs.Market.Trader.MaxLeverage)
+	}
+	if configs.Market.Trader.LossTolerance > 0 {
+		t.lossTolerance = big.NewDecimal(configs.Market.Trader.LossTolerance)
+	}
+	if configs.Market.Trader.ProfitMargin > 0 {
+		t.profitMargin = big.NewDecimal(configs.Market.Trader.ProfitMargin)
+	}
+	t.quoteCurrency = configs.Market.Base.Crypto.QuoteCurrency
+	return nil
 }
 
 // isConnected returns true when the trader is connected to other market participants, false otherwise.
 func (t *trader) isConnected() bool { return t.connected }
 
 // connect connects the trader to other market participants py listening to
-// decicated channels for communication.
+// decicated channels for communication. It is called only once on ititialization.
 func (t *trader) connect() {
 	t.Lock()
 	defer t.Unlock()
@@ -96,17 +135,158 @@ func (t *trader) connect() {
 	t.connected = true
 }
 
-func (t *trader) processEvaluatorRequest(msg *message) {
-	fmt.Println(msg)
+// getBinSpotBalances returns assets holding on Binance spot account.
+func (t *trader) getBinSpotBalances() []bn.Balance {
+	out := []bn.Balance{}
+	t.binSpotBalances.Range(func(key, value interface{}) bool {
+		out = append(out, value.(bn.Balance))
+		return true
+	})
+	return out
 }
 
-// streamingUserData manages all account changing events from trading activities on cash account.
+// updatebinSpotBalances removes or adds assset to holdings on trader binSpotBalances.
+// It is called once on initialization and upon receving data from websocket for UserData event.
+func (t *trader) updatebinSpotBalances(bl bn.Balance) {
+	t.Lock()
+	defer t.Unlock()
+	if big.NewFromString(bl.Free).EQ(big.ZERO) {
+		t.binSpotBalances.Delete(bl.Asset)
+		return
+	}
+	t.binSpotBalances.Store(bl.Asset, bl)
+}
+
+// isAllowedMarkets checks if the runner is allowed to trade based on its targered markets, which are SPOT or FUTURES.
+func (t *trader) isAllowedMarkets(r *runner.Runner) bool {
+	for _, m := range t.allowedMarkets {
+		if m == r.GetMarketType() {
+			return true
+		}
+	}
+	return false
+}
+
+// isAllowedPatterns checks if the runner is allowed to trade based on its ticker name.
+func (t *trader) isAllowedPatterns(r *runner.Runner) bool {
+	for _, p := range t.allowedPatterns {
+		isMatched, err := p.MatchString(r.GetName())
+		if err != nil {
+			t.logger.Error.Println(t.newLog(err.Error()))
+			continue
+		}
+		if isMatched {
+			return true
+		}
+	}
+	return false
+}
+
+// isHolding checks if the intended trading ticker is currently beign held.
+func (t *trader) isHolding(r *runner.Runner) bool {
+	valid := false
+	if r.GetMarketType() == runner.Cash {
+		t.binSpotBalances.Range(func(key, value interface{}) bool {
+			valid = strings.ToUpper(key.(string)+t.quoteCurrency) == strings.ToUpper(r.GetName())
+			return !valid
+		})
+	}
+	return valid
+}
+
+// initialCheck validates if a runner is currently allowed to be traded before placing an order to the markets.
+func (t *trader) initialChecks(r *runner.Runner) bool {
+	t.Lock()
+	defer t.Unlock()
+
+	if !t.isAllowedMarkets(r) {
+		return false
+	}
+	if !t.isAllowedPatterns(r) {
+		return false
+	}
+	if t.isHolding(r) {
+		return false
+	}
+	if val, ok := t.binSpotBalances.Load(t.quoteCurrency); !ok || !big.NewFromString(val.(bn.Balance).Free).GT(t.minBalance) {
+		return false
+	}
+	return true
+}
+
+// stopLoss calculates the stop loss price given a price.
+func (t *trader) stopLoss(price, tradingSide string) string {
+	quantity := t.minBalance.Div(big.NewFromString(price))
+	if strings.ToUpper(tradingSide) == "BUY" {
+		return t.minBalance.Mul(big.NewDecimal(1.0).Sub(t.lossTolerance)).Div(quantity).FormattedString(8)
+	}
+	return t.minBalance.Mul(big.NewDecimal(1.0).Add(t.lossTolerance)).Div(quantity).FormattedString(8)
+}
+
+// processEvaluatorRequest take care of the request from the evaluator, which will place trades on successfully evaluated signals.
+func (t *trader) processEvaluatorRequest(msg *message) {
+	if msg.request.what.runner == nil || msg.request.what.signal == nil {
+		return
+	}
+	if !t.initialChecks(msg.request.what.runner) {
+		return
+	}
+	r, s := msg.request.what.runner, msg.request.what.signal
+	if r.GetMarketType() == runner.Cash {
+		// TODO: think of the price and position sizing
+		// currently no position sizing as it uses predetermined price of minBalance
+		price := "30000"
+
+		// place a LIMIT order always.
+		order, err := t.provider.binSpot.NewCreateOrderService().
+			// set the symbol from the runner
+			Symbol(r.GetName()).
+			// set the trading side from the signal
+			Side(bn.SideType(s.TradingSide())).
+			// order type is always stop loss
+			Type(bn.OrderTypeLimit).
+			// timeInFore is always good-to-candle
+			TimeInForce(bn.TimeInForceTypeGTC).
+			// set the limit price
+			Price(price).
+			// quantity deduced from price and minimum trading balance for a position
+			Quantity(t.minBalance.Div(big.NewFromString(price)).FormattedString(8)).
+			// place the order
+			Do(context.Background())
+		if err != nil {
+			t.logger.Error.Println(t.newLog(err.Error()))
+			return
+		}
+		// after placing an order, the trader goes on to monitor the trade
+		fmt.Println(fmt.Sprintf("%+v", *order))
+	}
+}
+
+// binSpotUserDataStreaming manages all account changing events from trading activities on cash account.
 func (t *trader) binSpotUserDataStreaming() {
 	isError, isInit := false, true
-	errorHandler :=
-		func(err error) { t.logger.Error.Println(t.newLog(err.Error())); isError = true }
+	dataHandler := func(e *bn.WsUserDataEvent) {
+		switch e.Event {
+		case bn.UserDataEventTypeOutboundAccountPosition:
+			for _, a := range e.AccountUpdate {
+				t.updatebinSpotBalances(bn.Balance{
+					Asset:  a.Asset,
+					Free:   a.Free,
+					Locked: a.Locked,
+				})
+			}
+		case bn.UserDataEventTypeBalanceUpdate:
+			t.logger.Info.Println(t.newLog(fmt.Sprintf("cash %+v", *e)))
+		case bn.UserDataEventTypeExecutionReport:
+			t.logger.Info.Println(t.newLog(fmt.Sprintf("cash %+v", *e)))
+		case bn.UserDataEventTypeListStatus:
+			t.logger.Info.Println(t.newLog(fmt.Sprintf("cash %+v", *e)))
+		default:
+		}
+	}
+	errorHandler := func(err error) { t.logger.Error.Println(t.newLog(err.Error())); isError = true }
 	for isInit || isError {
-		done, _, err := bn.WsUserDataServe(t.binSpotListenKey, binSpotUserDatHandler, errorHandler)
+		done, _, err := bn.WsUserDataServe(t.binSpotListenKey, dataHandler, errorHandler)
 		if err != nil {
 			t.logger.Error.Println(t.newLog(err.Error()))
 		}
@@ -115,17 +295,13 @@ func (t *trader) binSpotUserDataStreaming() {
 	}
 }
 
-func binSpotUserDatHandler(e *bn.WsUserDataEvent) {
-	fmt.Println(e)
-}
-
-// streamingUserData manages all account changing events from trading activities on futures account.
+// binFutuUserDataStreaming manages all account changing events from trading activities on futures account.
 func (t *trader) binFutuUserDataStreaming() {
 	isError, isInit := false, true
-	errorHandler :=
-		func(err error) { t.logger.Error.Println(t.newLog(err.Error())); isError = true }
+	dataHandler := func(e *bnf.WsUserDataEvent) { t.logger.Info.Println(t.newLog(fmt.Sprintf("futu %+v", *e))) }
+	errorHandler := func(err error) { t.logger.Error.Println(t.newLog(err.Error())); isError = true }
 	for isInit || isError {
-		done, _, err := bnf.WsUserDataServe(t.binFutuListenKey, binFutuUserDataHandler, errorHandler)
+		done, _, err := bnf.WsUserDataServe(t.binFutuListenKey, dataHandler, errorHandler)
 		if err != nil {
 			t.logger.Error.Println(t.newLog(err.Error()))
 		}
@@ -134,8 +310,18 @@ func (t *trader) binFutuUserDataStreaming() {
 	}
 }
 
-func binFutuUserDataHandler(e *bnf.WsUserDataEvent) {
-	fmt.Println(e)
+// registerStreamingChannel registers the runners with the streamer in order to
+// recevie and consume candles broadcasted by data providor.
+func (t *trader) registerStreamingChannel(mem tdmember) bool {
+	doneStreamingRegister := false
+	var maxTries int
+	for !doneStreamingRegister && maxTries <= 3 {
+		resC := make(chan *payload)
+		t.communicator.trader2Streamer <- t.communicator.newMessage(mem.runner, nil, mem.channels, nil, resC)
+		doneStreamingRegister = (<-resC).what.dynamic.(bool)
+		maxTries++
+	}
+	return doneStreamingRegister
 }
 
 // generates a new log with the format for the watcher
--- internal/cmd/market/trader_test.go
+++ internal/cmd/market/trader_test.go
@@ -1,27 +1,59 @@
 package market
 
 import (
-	"fmt"
+	"io/ioutil"
 	"testing"
+	"time"
 
+	"follow.markets/internal/pkg/runner"
+	"follow.markets/internal/pkg/strategy"
 	"follow.markets/pkg/config"
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_Trader(t *testing.T) {
-	fmt.Println("start to test on trader")
 	path := "./../../../configs/deploy.configs.json"
 	configs, err := config.NewConfigs(&path)
 	assert.EqualValues(t, nil, err)
 
-<<<<<<< HEAD
 	trader, err := newTrader(initSharedParticipants(configs), configs)
-=======
-	trader, err := newTrader(initSharedParticipants(configs))
->>>>>>> ef7ad40c9ceae9b107e5317419f98cf377cda0f6
 	assert.EqualValues(t, nil, err)
 	assert.EqualValues(t, false, trader.isConnected())
-	//for {
-	//	time.Sleep(10 * time.Second)
-	//}
+
+	mem := tdmember{
+		runner: runner.NewRunner("BTCUSDT", runner.NewRunnerDefaultConfigs()),
+		channels: &streamingChannels{
+			bar:   nil,
+			trade: nil,
+			depth: make(chan interface{}, 10),
+		},
+	}
+	assert.EqualValues(t, nil, mem.channels.bar)
+}
+
+func Test_Trader_Evaluator(t *testing.T) {
+	path := "./../../../configs/deploy.configs.json"
+	configs, err := config.NewConfigs(&path)
+	assert.EqualValues(t, nil, err)
+
+	common := initSharedParticipants(configs)
+	trader, err := newTrader(common, configs)
+	assert.EqualValues(t, nil, err)
+	assert.EqualValues(t, false, trader.isConnected())
+
+	trader.connect()
+	assert.EqualValues(t, true, trader.isConnected())
+
+	r := runner.NewRunner("BTCUSDT", runner.NewRunnerDefaultConfigs())
+	assert.EqualValues(t, "BTCUSDT", r.GetName())
+
+	signalPath := "./../../../configs/signals/signal.json"
+	raw, err := ioutil.ReadFile(signalPath)
+	assert.EqualValues(t, nil, err)
+	s, err := strategy.NewSignalFromBytes(raw)
+	assert.EqualValues(t, nil, err)
+
+	common.communicator.evaluator2Trader <- common.communicator.newMessage(r, s, nil, nil, nil)
+
+	time.Sleep(time.Minute)
 }
