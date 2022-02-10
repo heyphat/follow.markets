dfcdc0609e3ad159a5aadec567c64db27bc94364
Phat Huynh <phat@kakao.com>
2022-02-10 13:47:29 +0000

    Standardize communicating messages between market participants

--- internal/cmd/market/communicator.go
+++ internal/cmd/market/communicator.go
@@ -2,6 +2,13 @@ package market
 
 import (
 	"time"
+
+	"github.com/google/uuid"
+
+	"follow.markets/internal/pkg/runner"
+	"follow.markets/internal/pkg/strategy"
+	tax "follow.markets/internal/pkg/techanex"
+	ta "github.com/itsphat/techan"
 )
 
 type communicator struct {
@@ -34,36 +41,88 @@ type message struct {
 }
 
 type payload struct {
-	id   string
+	requestID  uuid.UUID
+	responseID uuid.UUID
+
+	what data
 	when time.Time
-	what interface{}
 }
 
-func (c *communicator) newMessage(data interface{}, responseChannel chan *payload) *message {
+type data struct {
+	runner   *runner.Runner
+	signal   *strategy.Signal
+	channels *streamingChannels
+
+	dynamic interface{}
+}
+
+func (c *communicator) newMessage(
+	r *runner.Runner,
+	s *strategy.Signal,
+	cs *streamingChannels,
+	u interface{},
+	responseChannel chan *payload) *message {
 	return &message{
-		request:  c.newPayload(data),
+		request:  c.newPayload(r, s, cs, u).addRequestID(nil),
 		response: responseChannel,
 	}
 }
 
-func (c *communicator) newMessageWithPayloadID(id string, data interface{}, responseChannel chan *payload) *message {
-	return &message{
-		request:  c.newPayloadWithID(id, data),
-		response: responseChannel,
+func (c *communicator) newPayload(
+	r *runner.Runner,
+	s *strategy.Signal,
+	cs *streamingChannels,
+	u interface{}) *payload {
+	data := data{}
+	if r != nil {
+		data.runner = r
+	}
+	if s != nil {
+		data.signal = s
+	}
+	if cs != nil {
+		data.channels = cs
+	}
+	if u != nil {
+		data.dynamic = u
+	}
+	return &payload{
+		what: data,
+		when: time.Now(),
 	}
 }
 
-func (c *communicator) newPayload(data interface{}) *payload {
-	return &payload{
-		when: time.Now(),
-		what: data,
+func (pl *payload) addRequestID(id *uuid.UUID) *payload {
+	if id != nil {
+		pl.responseID = *id
+	} else {
+		pl.requestID = uuid.New()
 	}
+	return pl
 }
 
-func (c *communicator) newPayloadWithID(id string, data interface{}) *payload {
-	return &payload{
-		id:   id,
-		when: time.Now(),
-		what: data,
+func (pl *payload) addResponseID() *payload {
+	pl.responseID = uuid.New()
+	return pl
+}
+
+type streamingChannels struct {
+	bar   chan *ta.Candle
+	trade chan *tax.Trade
+	depth chan interface{}
+}
+
+func (scs *streamingChannels) close() {
+	if scs == nil {
+		return
+	}
+	if scs.bar != nil {
+		close(scs.bar)
+	}
+	if scs.trade != nil {
+		close(scs.trade)
+	}
+	if scs.depth != nil {
+		close(scs.depth)
 	}
 }
