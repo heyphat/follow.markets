833078fa74e60e16cbe1c0bd8eabea3752442ce1
Phat Huynh <phat@kakao.com>
2022-02-15 15:11:19 +0000

    Add time layout constant

--- internal/cmd/market/constant.go
+++ internal/cmd/market/constant.go
@@ -1,9 +1,21 @@
 package market
 
+import "fmt"
+
 const (
 	TRADER    = "trader"
 	WATCHER   = "watcher"
 	MANAGER   = "manager"
 	STREAMER  = "streamer"
 	EVALUATOR = "evaluator"
+
+	SimpleDateTimeFormat = "01/02/2006T15:04:05"
+	SimpleDateFormat     = "01/02/2006"
+
+	SimpleTimeFormat   = "15:04:05"
+	SimpleDateFormatV2 = "2006-01-02"
+)
+
+var (
+	simpleLayout = fmt.Sprint(SimpleDateFormatV2, "T", SimpleTimeFormat)
 )
