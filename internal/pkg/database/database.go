e20f4b24b25a549666c8ab5975a2ec1053187fb3
Phat Huynh <phat@kakao.com>
2022-02-21 12:49:31 +0000

    Init the selected database

--- internal/pkg/database/database.go
+++ internal/pkg/database/database.go
@@ -1,23 +1,40 @@
 package database
 
 import (
+	"strings"
+
 	"follow.markets/internal/pkg/runner"
 	"follow.markets/pkg/config"
 )
 
+// The database interface. Each type of database has it own implementation of these methods
+// that are called on the market package.
 type Client interface {
 	Disconnect()
 	IsInitialized() bool
+
+	// setup methods
 	InsertSetups(ss []*Setup) (bool, error)
 	InsertOrUpdateSetups(ss []*Setup) (bool, error)
 	GetSetups(r *runner.Runner, opts *QueryOptions) ([]*Setup, error)
 
+	// notification methods
 	InsertNotifications(ns []*Notification) (bool, error)
 }
 
+// Create a new db client.
 func NewClient(configs *config.Configs) Client {
-	if configs.Database.MongoDB != nil {
+	if strings.ToLower(configs.Database.Use) == "mongodb" && configs.Database.MongoDB != nil {
 		return newMongDBClient(configs)
 	}
-	return MongoDB{}
+	if strings.ToLower(configs.Database.Use) == "notion" && configs.Database.Notion != nil {
+		return newNotionClient(configs)
+	}
+	if configs.Database.MongoDB != nil {
+		return newMongDBClient(configs)
+	} else if configs.Database.Notion != nil {
+		return newNotionClient(configs)
+	} else {
+		return MongoDB{}
+	}
 }
