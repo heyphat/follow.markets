9d5b870a4d1cafa6b7239d52946c272b65d6c062
Phat Huynh <phat@kakao.com>
2022-02-25 04:17:31 +0000

    Add backtest summary

--- internal/cmd/market/backtest.go
+++ internal/cmd/market/backtest.go
@@ -1,6 +1,9 @@
 package market
 
 import (
+	"bytes"
+	"os"
+
 	ta "github.com/itsphat/techan"
 	"github.com/sdcoffey/big"
 
@@ -44,5 +47,23 @@ func newBacktest(bt *db.Backtest) *backtest {
 	return out
 }
 
-func (b backtest) Summary() {
+func (bt backtest) summary(file string) map[string]float64 {
+	sm := make(map[string]float64, 6)
+	sm["Profit"] = ta.TotalProfitAnalysis{}.Analyze(bt.rcs)
+	sm["PctGain"] = ta.PercentGainAnalysis{}.Analyze(bt.rcs)
+	//sm["PeriodProfit"] = ta.PeriodProfitAnalysis{Period: bt.bt.Start.Sub(bt.bt.End)}.Analyze(bt.rcs)
+	sm["TotalTrades"] = float64(len(bt.rcs.Trades))
+	sm["ProfitableTrades"] = ta.ProfitableTradesAnalysis{}.Analyze(bt.rcs)
+	sm["AverageProfit"] = ta.AverageProfitAnalysis{}.Analyze(bt.rcs)
+	if series, ok := bt.r.GetLines(bt.r.SmallestFrame()); ok {
+		sm["Buy&Hold"] = ta.BuyAndHoldAnalysis{
+			TimeSeries:    series.Candles,
+			StartingMoney: bt.balance.Float(),
+		}.Analyze(bt.rcs)
+	}
+	buffer := bytes.NewBufferString("")
+	_ = ta.LogTradesAnalysis{Writer: buffer}.Analyze(bt.rcs)
+	if err := os.WriteFile(file, buffer.Bytes(), 0444); err != nil {
+	}
+	return sm
 }
